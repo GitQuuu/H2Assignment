8e51b116f59dc7b070597189e043b0f1de1e098d
Qu <Quan0027@elec.techcollege.dk>
2020-01-07 09:16:09 +0100

    Starting on H2Assignment for evaluation

---
+++ H2Assignment/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+using System.Dynamic;
+
+namespace H2Assignment.Controllers
+{
+    class StudentsController
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
---
+++ H2Assignment/Views/Home/Index.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2Assignment.Views.Home
+{
+    class Index
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to Student Tracker");
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("1.Add student \n2.Get a list of students \n3.Update a student \n4.Delete a student");
+        }
+
+    }
+}
