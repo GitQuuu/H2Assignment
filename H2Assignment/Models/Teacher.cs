2548110bca62a39a6ec5b0538c07c0ab67aa04d3
Qu <Quan0027@elec.techcollege.dk>
2020-01-09 15:01:29 +0100

    add new class Teacher for composition

---
+++ H2Assignment/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2Assignment.Models
+{
+    class Teacher
+    {
+        public int Id { get; set; }     
+        public int Salary { get; set; }
+        
+    }
+}
